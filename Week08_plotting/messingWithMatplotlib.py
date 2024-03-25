47020900aa475b74fe9d6ef6f8c4036674f19803
Sharon Curley <sbrogancurley@gmail.com>
2024-03-25 05:59:19 +0000

    Update gitignore

--- Week08_plotting/messingWithMatplotlib.py
+++ Week08_plotting/messingWithMatplotlib.py
@@ -5,7 +5,7 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 
-''' 
+
 # LINE PLOT           
 xpoints = np.array (range(1,101))
 ypoints = xpoints * xpoints
@@ -14,21 +14,21 @@ plt.plot (xpoints, ypoints)
 plt.show ()
 
 # Save it
-# plt.savefig("Lecture 1.png")
+plt.savefig("Lecture 1.png")
 
 # Add labels
-# plt.plot (xpoints, ypoints, label = "xsquared")
-# plt.legend ()
-# plt.show ()
+plt.plot (xpoints, ypoints, label = "xsquared")
+plt.legend ()
+plt.show ()
 
 
 # Add straight line
-# plt.plot (xpoints, ypoints, label = "xsquared")
-# plt.plot (xpoints, xpoints, label = "straight", color= "blue")
-# plt.legend ()
-# plt.show ()
-'''
+plt.plot (xpoints, ypoints, label = "xsquared")
+plt.plot (xpoints, xpoints, label = "straight", color= "blue")
+plt.legend ()
+plt.show ()
 
+'''
 # SCATTER PLOT
 
 xpoints = np.array (range(1,101))
@@ -41,3 +41,4 @@ plt.legend ()
 randompoints = np.random.randint(1,1000,100)
 plt.scatter(xpoints, randompoints, label="random")
 plt.show ()
+'''
