60e22e38f660411ae52ad095958c08e98c8205ea
Sharon Curley <sbrogancurley@gmail.com>
2024-03-28 06:42:51 +0000

    week09_errors

--- Week09_Errors/showLogging.py
+++ Week09_Errors/showLogging.py
@@ -1,1 +1,40 @@
 # showLogging.py
+# Demonstrate logging
+# Attributes you can put in the basicConfig
+#   level  DEBUG INFO WARNING ERROR
+#   filename
+#   filemode
+#   format
+#       %(name)s - %(levelname)s - %(message)s -  %(asctime)s - %(lineno)d
+
+import logging
+
+'''
+logging.basicConfig(level=logging.DEBUG)
+# program does stuff
+logging.debug ("we are doing stuff")               # Result = DEBUG:root:we are doing stuff
+'''
+
+'''
+logging.basicConfig(level=logging.WARNING)           # DEBUG shows all messages, changing to WARNING only shows warning, error & critical.
+# program does stuff
+logging.debug ("we are doing stuff")               # Result = DEBUG:root:we are doing stuff
+logging.info("This is information")
+logging.warning("I am not certain about this")
+logging.error("Not good")
+logging.critical("Really bad")
+'''
+
+
+logging.basicConfig(filename="./mainlog.log",
+                    level=logging.DEBUG,              # DEBUG shows all messages, changing to WARNING only shows warning, error & critical.
+                    format="(%(asctime)s-%(levelname)s-%(message)s-%(lineno)d")           
+
+# program does stuff
+logging.debug ("we are doing stuff")               # Result = DEBUG:root:we are doing stuff
+logging.info("This is information")
+logging.warning("I am not certain about this")
+logging.error("Not good")
+logging.critical("Really bad")
+
+
--- Week09_Errors/testMyFunctions.py
+++ Week09_Errors/testMyFunctions.py
@@ -1,3 +1,8 @@
 # testMyFunctions.py
-#
+## test the module myfunctions
 # Author: Sharon Curley (lecture notes)
+
+import myFunctions as mf 
+
+print (mf.withdraw(30))
+print ("all good")
