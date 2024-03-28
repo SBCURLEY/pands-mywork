41ed892a70537f2eace0c5fe21eb38c0f0a738b4
Sharon Curley <sbrogancurley@gmail.com>
2024-03-28 08:06:19 +0000

    week09_errors

---
+++ Week09_Errors/lab9.0- myFunctions.py
@@ -0,0 +1,10 @@
+# lab9.0- myFunctions.py
+# This program is to show how to make a module,  
+# that contains functions, and test cases for the functions
+# it has a function called fibonacci that takes in number
+# and returns a list containing a fibonacci sequence with that many numbers
+
+# A Fibonacci sequence is a series of numbers that starts off with 0 and 1 and each 
+# subsequence number is the sum of the previous two. 
+
+
